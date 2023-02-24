008e2cba849842ff8e3d26d71320530f996e29b5
jls28 <40491400+jls28@users.noreply.github.com>
2023-02-24 21:09:19 +0100

    Delete 142 directory

--- 142/Hello.java
+++
@@ -1,9 +0,0 @@
-// CSE 142 Lecture 1
-// Classes, Printing, Escape Sequences
-
-// Our first program. Following tradition, we print 'Hello world!'.
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-}
