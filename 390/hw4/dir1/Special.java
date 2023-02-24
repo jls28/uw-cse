0ad8f9bdb9ab796f23be5c346d8f90f25cf1ae33
jls28 <40491400+jls28@users.noreply.github.com>
2023-02-24 21:12:08 +0100

    Delete 390 directory

--- 390/hw4/dir1/Special.java
+++
@@ -1,8 +0,0 @@
-
-public class Special {
-    public static void main(String[] args) {
-        System.out.println();
-	System.out.println("This is a very special message.");
-	System.out.println("Do you agree?");
-    }
-}
